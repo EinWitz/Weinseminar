c7c09581803632e6551782d7a49a34e572f78d56
Karl Johann Schubert <kajo@kjschubert.de>
2018-03-06 12:07:51 +0100

    init terraform repo for jupyter server

---
+++ Jupyter Server/require.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.10.4"
+}
+
+provider "inwx" {
+  username = "${var.inwx_user}"
+  password = "${var.inwx_pw}"
+}
+
+provider "hcloud" {
+  token = "${var.hcloud_token}"
+}
